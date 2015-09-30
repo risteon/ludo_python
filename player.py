323fd62de3c9fe063c00be2d8407ae3967892053
Christoph B. Rist <rist.christoph@gmail.com>
2015-09-30 21:06:05 +0200

    [Player] Fix import and assertion

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 # Player
 
-from common_definitions import BoardFieldType, BoardFieldCount, PlayerCount, PawnCount, Players
+from common_definitions import Players
 
 
 class Player:
 
     def __init__(self, player):
+        assert player in Players
+
         self.player = player
         print("New player: ", player)
 
