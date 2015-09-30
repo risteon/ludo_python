5f100748bd3703f0ea3f1e12e85ffcb836fe1e97
Christoph B. Rist <rist.christoph@gmail.com>
2015-09-30 21:08:40 +0200

    [Die] Using common_definitions constant

--- die.py
+++ die.py
@@ -1,10 +1,11 @@
 import random
+from common_definitions import MAX_DICE_NUMBER_OF_POINTS
 
 
 class Die:
     """Simulate a 6-sided die."""
     def roll( self ):
-        self.value = random.randint(1,6)
+        self.value = random.randint(1, MAX_DICE_NUMBER_OF_POINTS)
         return self.value
 
     def getValue( self ):
