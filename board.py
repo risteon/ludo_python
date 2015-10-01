178118d1835852a693bf1da4b8b028b5af4c366e
Christoph B. Rist <rist.christoph@gmail.com>
2015-10-01 20:09:51 +0200

    [Board] Fixing field tuple return values, namedtuple

--- board.py
+++ board.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python3
 
+from collections import namedtuple
+
 from common_definitions import BoardFieldType, BOARD_FIELD_COUNT,\
                                PAWN_COUNT, Players, MAX_DICE_NUMBER_OF_POINTS
 
+"""
+Field definition: (BoardFieldType | Player | Index)
+"""
+Field = namedtuple('Field', ['type', 'player', 'index'])
 
-class BoardField:
+class BoardFieldDesc:
     def __init__(self, player):
         assert 0 <= player <= len(Players)
         # init with given player
@@ -24,11 +30,11 @@ class Board:
 
         for player in range(len(Players)):
             for index in range(PAWN_COUNT):
-                self._fields[(BoardFieldType.FINISH, player, index)] = BoardField(len(Players))
-                self._fields[(BoardFieldType.HOME, player, index)] = BoardField(player)
+                self._fields[(BoardFieldType.FINISH, player, index)] = BoardFieldDesc(len(Players))
+                self._fields[(BoardFieldType.HOME, player, index)] = BoardFieldDesc(player)
 
         for index in range(BOARD_FIELD_COUNT):
-            self._fields[(BoardFieldType.FIELD, len(Players), index)] = BoardField(len(Players))
+            self._fields[(BoardFieldType.FIELD, len(Players), index)] = BoardFieldDesc(len(Players))
 
         # pawns
         self.pawns = []
@@ -37,36 +43,45 @@ class Board:
         for player in Players:
             pawns = []
             for i in range(PAWN_COUNT):
-                pawns.append((BoardFieldType.HOME, player, i))
+                pawns.append(Field(BoardFieldType.HOME, player, i))
             self.pawns.append(pawns)
 
-    def get_next_field(self, player, from_field, dice_dots):
+    def get_next_field(self, player, from_field, number_of_points):
         if from_field[0] is BoardFieldType.HOME:
-            if dice_dots is not MAX_DICE_NUMBER_OF_POINTS:
+            if number_of_points is not MAX_DICE_NUMBER_OF_POINTS:
                 return False
             return self._get_player_start_field(player)
 
         elif from_field[0] is BoardFieldType.FINISH:
-            target_field = from_field[1] + dice_dots
+            target_field = from_field[2] + number_of_points
             if target_field > (PAWN_COUNT - 1):
                 return False
-            return BoardFieldType.FINISH, target_field
+            return Field(BoardFieldType.FINISH, player, target_field)
 
         elif from_field[0] is BoardFieldType.FIELD:
-            player_field_counter = Board._make_player_field_counter(player, from_field[1])
-            target = player_field_counter + dice_dots
+            player_field_counter = Board._make_player_field_counter(player, from_field[2])
+            target = player_field_counter + number_of_points
             if target < BOARD_FIELD_COUNT:
-                return BoardFieldType.FIELD, Board._make_global_field_counter(player, target)
+                return Field(BoardFieldType.FIELD,
+                             len(Players),
+                             Board._make_global_field_counter(player, target))
 
             return self.get_next_field(player,
-                                       (BoardFieldType.FINISH, 0),
+                                       (BoardFieldType.FINISH, player, 0),
                                        target - BOARD_FIELD_COUNT)
 
         assert False
 
+    def get_board_field_desc(self, field):
+        """ Getter for _field array
+        :param field:
+        :return:
+        """
+        return self._fields[field]
+
     @staticmethod
     def _get_player_start_field(player):
-        return BoardFieldType.FIELD, len(Players), player * BOARD_FIELD_COUNT / len(Players)
+        return Field(BoardFieldType.FIELD, len(Players), player * BOARD_FIELD_COUNT / len(Players))
 
     @staticmethod
     def _make_player_field_counter(player, global_field_counter):
