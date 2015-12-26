63d1c18872d3491a6006fad40fd31ae9317491b4
Christoph B. Rist <rist.christoph@gmail.com>
2015-12-26 22:08:54 +0100

    Add empty unittest class

---
+++ ludo/ludo_test.py
@@ -0,0 +1,13 @@
+from move_manager import MoveManager
+from board import Board, Field
+
+import unittest
+
+class TestMoves(unittest.TestCase):
+
+    def test_valid_moves(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
