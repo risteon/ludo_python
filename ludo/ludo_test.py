57d1053293424a23f3a74691d743e043f379b1e8
Christoph B. Rist <rist.christoph@gmail.com>
2015-12-26 22:26:56 +0100

    Add test case for no valid move in finish bug

--- ludo/ludo_test.py
+++ ludo/ludo_test.py
@@ -1,12 +1,23 @@
-from move_manager import MoveManager
+from move_manager import MoveManager, Move
 from board import Board, Field
+from common_definitions import BoardFieldType, BOARD_FIELD_COUNT,\
+                               PAWN_COUNT, Players, MAX_DICE_NUMBER_OF_POINTS
 
 import unittest
 
+
 class TestMoves(unittest.TestCase):
 
-    def test_valid_moves(self):
-        pass
+    def test_valid_moves_in_finish(self):
+        board = Board()
+        move_manager = MoveManager(board)
+
+        target = Field(type=BoardFieldType.FINISH, player=Players.green, field_index=0)
+        board.move_pawn(Players.green, 0, target)
+
+        moves = move_manager.get_valid_moves(Players.green, 1)
+
+        self.assertNotEqual(moves, False, "No valid moves found")
 
 
 if __name__ == '__main__':
