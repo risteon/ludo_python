fe479bf2a8ec547922c6643bbdf0ba768eb79c9d
Christoph B. Rist <rist.christoph@gmail.com>
2015-12-29 00:41:11 +0100

    Add script to simulate multiple games

---
+++ ludo/simulator.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+from game import Game
+
+print("Welcome to a game of ludo!")
+
+average_throw_counter = 0
+min_throws_per_game = 10000000
+max_throws_per_game = 0
+NUM_GAMES = 100
+
+for i in range(0, NUM_GAMES):
+
+    game = Game()
+    throw_counter = 0
+
+    while game.next_move():
+        throw_counter += 1
+
+    average_throw_counter += throw_counter
+    if throw_counter < min_throws_per_game:
+        min_throws_per_game = throw_counter
+    if throw_counter > max_throws_per_game:
+        max_throws_per_game = throw_counter
+
+    print("Game:", i+1)
+
+print("Average throws:", average_throw_counter/NUM_GAMES)
+print("Min", min_throws_per_game)
+print("Max", max_throws_per_game)
+
+
